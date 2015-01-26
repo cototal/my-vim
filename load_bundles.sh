65ab4345e15f31f111fd1df5d032ea915310175a
Shaun Martin <martins@surgery.wisc.edu>
2015-01-26 08:31:45 -0600

    Add macro file and rails bundle.

--- load_bundles.sh
+++ load_bundles.sh
@@ -6,6 +6,7 @@ rm -rf bundle/nerdtree
 rm -rf bundle/emmet-vim
 rm -rf bundle/vim-kolor
 rm -rf bundle/tabular
+rm -rf bundle/vim-rails
 
 git clone https://github.com/tpope/vim-surround bundle/vim-surround
 git clone https://github.com/tpope/vim-commentary bundle/vim-commentary
@@ -13,3 +14,4 @@ git clone https://github.com/scrooloose/nerdtree bundle/nerdtree
 git clone https://github.com/mattn/emmet-vim bundle/emmet-vim
 git clone https://github.com/zeis/vim-kolor bundle/vim-kolor
 git clone git://github.com/godlygeek/tabular.git bundle/tabular
+git clone git://github.com/tpope/vim-rails.git
